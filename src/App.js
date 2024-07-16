de96a775b242ba77a405808c10f300c29adb4464
sonomastea <sbstea@usf.edu>
2024-07-16 16:00:31 -0400

    app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import MovieSelector from '/movieselector.js'
+import UploadButton from '/upload.js'
+import "/app.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+<div className='App'>
+  <Title >Interactive Media Toolbar Enhancment</Title>
+
+  <MovieSelector/>
+  <Upload/>
+</div>
+export default App
