815ca8ab47ea3cb33e7264a7a918686ab2bf4468
sonomastea <sbstea@usf.edu>
2024-07-16 23:45:35 -0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import RateMovie from './ratemovie'
 import MovieSelector from '/movieselector.js'
 import UploadButton from '/upload.js'
 import "/app.css"
+export default App{
 const [selectedMovie, setSelectedMovie]= useState(null);
 const setMovie=(moviename) =>
   setSelectedMovie(moviename);
-export default App{
 
-  return(
 <div className="MovieSelector">
 <title>Enhanced Movie Selector</title>
   <MovieSelector onSelectMovie={handleselectedmovie}/>
 };
-
-
   {selectedMovie && (
-  RateMovieButton moviename={selectedMovie.name}
+  RateMovieButton = (handlemovierate).name}
+  
+  </Upload Button>
 
+  </div>
   )};
 
 );
