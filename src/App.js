36e74f21b88d07d9558b83b3046f7e1bd344dde0
sonomastea <sbstea@usf.edu>
2024-07-16 23:32:43 -0400

    updata app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 //U05721483
 import React, {useState} from 'react'
+import RateMovie from './ratemovie'
 import MovieSelector from '/movieselector.js'
 import UploadButton from '/upload.js'
 import "/app.css"
+const [selectedMovie, setSelectedMovie]= useState(null);
+const setMovie=(moviename) =>
+  setSelectedMovie(moviename);
+export default App{
+
+  return(
+<div className="MovieSelector">
+<title>Enhanced Movie Selector</title>
+  <MovieSelector onSelectMovie={handleselectedmovie}/>
+};
 
 
+  {selectedMovie && (
+  RateMovieButton moviename={selectedMovie.name}
 
-<div className='App'>
-  <Title >Interactive Media Toolbar Enhancment</Title>
+  )};
 
-  <MovieSelector/>
-  <Upload/>
-</div>
-export default App
+);
